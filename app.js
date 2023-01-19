700ed2ba045e5bdb858444c7c3760a5fe20f1911
SuchithG <suchithgowda1995@gmail.com>
2023-01-20 00:08:45 +0530

    feat: :construction: Express basic routes

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express();
 
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Hello from the server side !", app: "Natours" });
+});
+
+app.post("/", (req, res) => {
+  req.send("You can post this to your endpoint...");
+});
+
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
