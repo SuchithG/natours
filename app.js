1b429779bf532c722185717f4c1e9a8635d694dd
SuchithG <suchithgowda1995@gmail.com>
2023-01-21 20:51:15 +0530

    feat: :construction: API Handling GET Requests

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side !", app: "Natours" });
-});
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side !", app: "Natours" });
+// });
 
-app.post("/", (req, res) => {
-  req.send("You can post this to your endpoint...");
+// app.post("/", (req, res) => {
+//   req.send("You can post this to your endpoint...");
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/starter/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
