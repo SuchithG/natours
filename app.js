4a07efb7b6c9504a149f1542d38e58dd4ad12260
SuchithG <suchithgowda1995@gmail.com>
2023-01-25 23:56:25 +0530

    feat: :construction: Handling DELETE requests

--- app.js
+++ app.js
@@ -90,6 +90,20 @@ app.patch("/api/v1/tours/:id", (req, res) => {
   });
 });
 
+app.delete("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
