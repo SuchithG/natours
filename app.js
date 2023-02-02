38427e4c02da15bf1eb110f92b20642e23dbe728
SuchithG <suchithgowda1995@gmail.com>
2023-02-03 00:58:13 +0530

    feat: :construction: Serving static files

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
-// 1) MIDDLEWARES
 const app = express();
 
+// 1) MIDDLEWARES
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from the middleware");
