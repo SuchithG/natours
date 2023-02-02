77271b6522139e9f4e342c699979b9f1a1173e77
SuchithG <suchithgowda1995@gmail.com>
2023-02-03 00:15:40 +0530

    feat: :sparkles: Param Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../starter/dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
@@ -59,13 +70,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid Id",
-    });
-  }
-
   res.status(200).json({
     status: "success",
     data: {
@@ -75,13 +79,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid Id",
-    });
-  }
-
   res.status(204).json({
     status: "success",
     data: null,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,9 @@
+const { Router } = require("express");
 const express = require("express");
 const tourController = require("./../controllers/tourController");
 const tourRouter = express.Router();
+
+tourRouter.param("id", tourController.checkID);
 
 tourRouter
   .route("/")
