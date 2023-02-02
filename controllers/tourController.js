873ddde2a57367809b9df7acc6ee4acb77c5b125
SuchithG <suchithgowda1995@gmail.com>
2023-02-03 00:33:29 +0530

    feat: :zap: Chaining multiple middlewares

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Missing name or price",
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
@@ -59,7 +69,7 @@ exports.createTour = (req, res) => {
     `${__dirname}/starter/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      res.sendStatus(201).json({
+      res.status(201).json({
         status: "success",
         data: {
           tour: newTour,
