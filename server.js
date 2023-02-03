de93f9984cecf96ebf3466fc49601f603b637ec9
SuchithG <suchithgowda1995@gmail.com>
2023-02-03 18:58:26 +0530

    feat: :sparkles: Setting up environment variables

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
 const app = require("./app");
 
-const port = 3003;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
